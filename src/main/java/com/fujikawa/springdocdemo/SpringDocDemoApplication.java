99a5df58f57116d25d870ef37f762eb978b2cb44
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 15:55:16 -0300

    Criação do projeto

---
+++ src/main/java/com/fujikawa/springdocdemo/SpringDocDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springdocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocDemoApplication.class, args);
+	}
+
+}
