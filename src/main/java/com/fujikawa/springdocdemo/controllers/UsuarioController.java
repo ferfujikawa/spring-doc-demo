ca82a0e47a8a900d0e296dc73819ec7c1ae32359
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 16:29:38 -0300

    Criação de controller de Usuário com endpoint GET para listar usuários.

---
+++ src/main/java/com/fujikawa/springdocdemo/controllers/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.fujikawa.springdocdemo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fujikawa.springdocdemo.dtos.UsuarioDTO;
+
+@RestController
+@RequestMapping("usuarios")
+public class UsuarioController {
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+
+        List<UsuarioDTO> usuarios = Arrays.asList(
+            new UsuarioDTO(UUID.randomUUID(), "Usuário 1", "usuario1@email.com"),
+            new UsuarioDTO(UUID.randomUUID(), "Usuário 2", "usuario2@email.com"),
+            new UsuarioDTO(UUID.randomUUID(), "Usuário 2", "usuario2@email.com")
+        );
+
+        return ResponseEntity.ok(usuarios);
+    }
+}
