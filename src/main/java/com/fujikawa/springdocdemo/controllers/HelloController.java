54fb659f146c8d4a24ddfc61d05589eed753b963
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-19 16:08:02 -0300

    Criação do HelloController com endpoint GET para endereço raiz.

---
+++ src/main/java/com/fujikawa/springdocdemo/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.fujikawa.springdocdemo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+
+        return ResponseEntity.ok("Olá");
+    }
+}
